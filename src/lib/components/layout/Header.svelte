213f0c406abd4a3df52b3fd0eefabe72e3cf87d1
Brendan C. Ward <bcward@astutespruce.com>
2025-05-05 11:20:04 -0700

    Fix base path in header

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,13 +1,15 @@
 <script>
+    import { base } from '$app/paths'
+
     import Logo from '$lib/assets/SECAS_logo.svg'
 </script>
 
-<header class="flex flex-none items-center gap-3 py-2 px-2 leading-none border-b-2 border-zinc-600">
+<header class="flex flex-none items-center gap-3 border-b-2 border-zinc-600 px-2 py-2 leading-none">
     <a href="https://secassoutheast.org/" target="_blank" class="text-foreground hover:no-underline">
         <img src={Logo} height="30px" width="30px" alt="SECAS logo" />
     </a>
 
-    <a href="/">
+    <a href={`${base}/`}>
         <h1 class="m-0 text-2xl leading-none">Southeast Conservation Adaptation Strategy Projects</h1>
     </a>
 </header>
